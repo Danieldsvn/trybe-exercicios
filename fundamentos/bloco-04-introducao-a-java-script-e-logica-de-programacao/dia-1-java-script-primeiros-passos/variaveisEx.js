cb7dbb9deab5e942cfc7a5641a2f49033397a56c
Daniel Saraiva <danieldsvn@gmail.com>
2021-11-26 15:20:52 -0300

    primeira atividade do dia concluido

---
+++ fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-1-java-script-primeiros-passos/variaveisEx.js
@@ -0,0 +1,11 @@
+const myName = "Daniel";
+const birthCity = "Fortaleza";
+let birthYear = 1990
+
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+birthYear = 2030;
+console.log(birthYear);
+birthCity = "Maracanau";
