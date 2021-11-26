b805a53935413c9999837ceda61837297a8e4999
Daniel Saraiva <danieldsvn@gmail.com>
2021-11-26 19:26:46 -0300

    atividade 4 concluida

---
+++ fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-1-java-script-primeiros-passos/switch-case.js
@@ -0,0 +1,14 @@
+let resultado = "reprovada";
+switch(resultado) {
+    case "aprovada":
+        console.log(resultado);
+        break;
+    case "lista":
+      console.log(resultado);
+      break;
+    case "reprovada":
+      console.log(resultado);
+      break;
+    default:
+        console.log("não se aplica");  
+}
