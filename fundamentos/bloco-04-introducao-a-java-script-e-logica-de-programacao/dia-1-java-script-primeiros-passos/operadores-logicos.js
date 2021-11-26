52ce17ce4ba466744f89c73151ef2aca8520a49a
Daniel Saraiva <danieldsvn@gmail.com>
2021-11-26 19:12:05 -0300

    atividade 4 concluida

---
+++ fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-1-java-script-primeiros-passos/operadores-logicos.js
@@ -0,0 +1,42 @@
+//const comida = 'pão na chapa';
+//const bebida = 'cafézinho';
+
+//if (bebida === 'cafézinho' && comida === 'pão na chapa') {
+//  console.log('Muito obrigado pela refeição :)');
+//} else {
+//  console.log('Acho que houve um engano com meu pedido');
+//}
+//const conditionOne = true;
+//const conditionTwo = false;
+
+//console.log(conditionOne && conditionTwo);
+//console.log(true && true); // true
+//console.log(true && false); // false
+//console.log(false && true); // false
+//console.log(false && false); // false
+
+//const currentHour = 7;
+//let message = "";
+/* if (currentHour >= 22) {
+    message = "Não deveríamos comer nada, é hora de dormir";
+} 
+else if (currentHour >= 18 && currentHour < 22) {
+    message = "Rango da noite, vamos jantar :D";
+}
+else if (currentHour >= 14 && currentHour < 18) {
+    message = "Vamos fazer um bolo para o café da tarde?";
+}
+else if (currentHour >= 11 && currentHour < 14) {
+    message = "Hora do almoço!!!";
+}
+else if (currentHour >= 4 && currentHour < 11 ) {
+    message = "Hmm, cheiro de café recém passado";
+}
+console.log(message); */
+let weekDay = "sábado";
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+    console.log("Oba, mais um dia de aprendizado na TRYBE >:D");
+}
+else if (weekDay === "sábado" || weekDay === "domingo" ) {
+    console.log("FINALMENTE, descanso merecido UwU");
+}
