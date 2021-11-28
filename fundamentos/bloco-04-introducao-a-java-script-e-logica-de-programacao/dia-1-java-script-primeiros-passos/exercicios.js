91ca3ae8e9535ccbc137c25984d7e3c59399c2b8
Daniel Saraiva <danieldsvn@gmail.com>
2021-11-28 19:59:13 -0300

    etapa 2 concluida

--- fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-1-java-script-primeiros-passos/exercicios.js
+++ fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-1-java-script-primeiros-passos/exercicios.js
@@ -6,13 +6,32 @@ Multiplicação (a * b)
 Divisão (a / b)
 Módulo (a % b)*/
 
-//Adição
+/*Adição
 const a = 3;
 const b = 4;
 console.log("adição: " + (a + b));
 console.log('subtração: ' + (a - b));
 console.log('multiplicação: ' + (a * b));
 console.log('divisão: ' + (a / b));
-console.log('módulo: ' + (a % b));
+console.log('módulo: ' + (a % b));*/
 
+/* Exercício 2
+Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.*/
 
+/* const a = 8;
+const b = 8;
+if( a > b) {
+    console.log(a + " é o maior número ");
+}
+else {
+    console.log(b + " é o maior número");
+} */
+
+const a = 20;
+const b = 21;
+
+if (a > b) {
+  console.log("'a' é maior que 'b'");
+} else {
+  console.log("'b' é maior que 'a'");
+};
