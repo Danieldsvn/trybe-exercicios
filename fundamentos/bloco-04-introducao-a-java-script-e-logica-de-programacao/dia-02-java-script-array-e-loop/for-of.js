29d234784d6fe8257b49ff1c8a9dd6f275ae44d8
Daniel Saraiva <danieldsvn@gmail.com>
2021-11-29 11:17:27 -0300

    exercicio for-of realizado

---
+++ fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-02-java-script-array-e-loop/for-of.js
@@ -0,0 +1,33 @@
+
+/* let numeros = [1,2,3,4,5,'arroz',6];
+for(let numero of numeros) {
+  console.log(numero);
+} */
+
+// resultado: 
+//1
+//2
+//3
+//4
+//5 
+
+/* let word = "tapioquinha";
+for(let letter of word) {
+    console.log(letter);
+} */
+
+
+/* let arrOfNumbers = [10, 20, 30];
+for (let soma2 of arrOfNumbers) {
+  soma2 += 2;
+  console.log(soma2);
+} */
+// 11
+// 21
+// 31
+// ! Exercício 1 - Utilize o for/of para imprimir os elementos da lista names com o console.log() :
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let elementos of names) {
+        console.log(names);
+}
