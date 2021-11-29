51ca6ac29ab892b6811cf9a80061fab21eb908b7
Daniel Saraiva <danieldsvn@gmail.com>
2021-11-29 10:55:05 -0300

    exercicio arrays e loop for finalizado

---
+++ fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-02-java-script-array-e-loop/arrays-e-loop-for.js
@@ -0,0 +1,15 @@
+/* let cars = ["Saab", "Volvo", "BMW"];
+
+for (let index = 0; index < cars.length; index += 1) {
+  console.log(cars[index]);
+} */
+
+// ! Exercício 1
+// ! Utilize o for para imprimir os elementos da lista groceryList com o console.log()
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]);
+    console.log('-');
+}
