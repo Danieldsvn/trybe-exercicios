5db6cd64076f19a3d4983c391b29d5be91d8a32e
Daniel Saraiva <danieldsvn@gmail.com>
2021-11-29 10:33:05 -0300

    exercicios do arrays-lista.js concluidos

---
+++ fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-02-java-script-array-e-loop/arrays-lista.js
@@ -0,0 +1,36 @@
+/* let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+tasksList.unshift('Fazer exercícios da Trybe');  // adiciona mais uma tarefa no início do array
+console.log(tasksList); */
+
+// ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
+
+/*  let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+*tasksList.pop();  // remove ultimo elemento do array
+*tasksList.push('Estudar no course'); // adiciona elemento ao array na última posição.
+*tasksList.unshift('Aprimorar portfólio'); // adiciona elemento ao array na primeira posição
+console.log(tasksList);
+lastTask = tasksList[tasksList.length - 1];
+*console.log(tasksList.indexOf('Estudar no course')); // informa índice de determinado elemento no array
+console.log(lastTask); */
+
+// ! Exercício 1 - Obtenha o valor "Serviços" do array menu :
+
+/* let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices); */
+
+// ! Exercício 2 - Procure o índice do valor "Portfólio" do array menu :
+
+/* let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+ */
+// ! Exercício 3 -Adicione o valor "Contato" no final do array menu :
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato'); 
+console.log(menu);
