ae20e056b08f9eef30b3f9f13ab48883de05609f
Daniel Saraiva <danieldsvn@gmail.com>
2021-11-28 21:27:13 -0300

    exercicio 7 concluido

--- fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-1-java-script-primeiros-passos/exercicios.js
+++ fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-1-java-script-primeiros-passos/exercicios.js
@@ -111,7 +111,7 @@ Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
 Exemplo: bishop (bispo) -> diagonals (diagonais).*/
 
  //Solução:
-chessPieceAnyCase = "KNIGHT";
+/* chessPieceAnyCase = "KNIGHT";
 chessPiece = chessPieceAnyCase.toLowerCase();
  if (chessPiece == "pawn") {
      console.log("vertically forward one square");
@@ -133,4 +133,40 @@ else if (chessPiece == "king") {
 }
 else {
     console.log("Erro: peça inválida")
+} */
+/*Exercicio 7
+ Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+Porcentagem >= 90 -> A
+Porcentagem >= 80 -> B
+Porcentagem >= 70 -> C
+Porcentagem >= 60 -> D
+Porcentagem >= 50 -> E
+Porcentagem < 50 -> F
+O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.*/
+
+let porcentagem = -1;
+
+if ( porcentagem >= 90 && porcentagem <= 100) {
+    console.log("A");
 }
+else if ( porcentagem >= 80 && porcentagem < 90) {
+    console.log("B");
+}
+else if ( porcentagem >= 70 && porcentagem < 80) {
+    console.log("C");
+}
+else if ( porcentagem >= 60 && porcentagem < 70) {
+    console.log("D");
+}
+else if ( porcentagem >= 50 && porcentagem < 60) {
+    console.log("E");
+}
+else if (porcentagem >= 0 && porcentagem < 50) {
+    console.log("F");
+}
+else {
+    console.log("Valor inválido. Fechando programa...")
+}
+
+
+
