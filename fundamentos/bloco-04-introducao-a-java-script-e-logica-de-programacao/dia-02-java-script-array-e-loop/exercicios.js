63ffa4c19a46175e926b33dbc0b6de078a2d9da7
Daniel Saraiva <danieldsvn@gmail.com>
2021-11-29 12:19:49 -0300

    etapa 1 concluida

---
+++ fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-02-java-script-array-e-loop/exercicios.js
@@ -0,0 +1,15 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// ! 1 - Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+
+//*Solução 1
+/* for (let i = 0; i < numbers.length; i += 1 ) {
+    console.log(numbers[i]);
+} */
+//*Solução 2
+/* for (let i of numbers) {
+    console.log(i);
+}
+ */
+
+
