76ce2dd911ee99b682ea066d50ef3e05bacf2859
Daniel Saraiva <danieldsvn@gmail.com>
2021-11-29 12:55:47 -0300

    exercicio 5 concluido

--- fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-02-java-script-array-e-loop/exercicios.js
+++ fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-02-java-script-array-e-loop/exercicios.js
@@ -50,10 +50,10 @@ console.log(mean); */
 
 // ! 4 - Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
 
 //*Solução 1
- let sum = 0;
+ /* let sum = 0;
 
 for (let i = 0; i < numbers.length; i += 1) {
     sum = sum + numbers[i];
@@ -65,8 +65,20 @@ if (mean > 20) {
 }
 else {
     console.log('valor menor ou igual a 20');
-}
+} */
 
 //*Solução 2
 
+// ! 5 - Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//*Solução 1
+let min = numbers[0];
+for (let i = 0; i < numbers.length; i += 1) {
+    if ( min > numbers[i] ) {
+        min = numbers[i]; 
+    }
+} 
+console.log(min);
+//*Solução 2
