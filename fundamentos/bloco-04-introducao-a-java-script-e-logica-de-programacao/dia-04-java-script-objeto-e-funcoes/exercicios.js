001f921b942a3c726fe49837450a5f9753683f69
Daniel Saraiva <danieldsvn@gmail.com>
2021-12-01 20:47:03 -0300

    exercicios do fixar for/in realizados com sucesso

--- fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-04-java-script-objeto-e-funcoes/exercicios.js
+++ fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-04-java-script-objeto-e-funcoes/exercicios.js
@@ -1,4 +1,4 @@
-/*  Para fixar
+/*  PARA FIXAR "OBJETO"
 Agora vamos fazer alguns exercícios de fixação para consolidar os conhecimentos adquiridos no video anterior! 😉  */
 
 // ! 1 - Crie um objeto player contendo as variáveis listadas abaixo.
@@ -8,15 +8,15 @@ Agora vamos fazer alguns exercícios de fixação para consolidar os conheciment
 // let age = 34;
 // let medals = { golden: 2, silver: 3 };
 
-let player = {
-    name: 'Marta',
-    lastName: 'Silva',
-    age: '34',
-    medals: {
-        golden: 2,
-        silver: 3,
-    },
-};
+// let player = {
+//     name: 'Marta',
+//     lastName: 'Silva',
+//     age: '34',
+//     medals: {
+//         golden: 2,
+//         silver: 3,
+//     },
+// };
 
 // ! 2 - Acesse as chaves name , lastName e age e concatene as suas informações para imprimir no console uma mensagem no seguinte formato: "A jogadora Marta Silva tem 34 anos de idade".
 
@@ -25,7 +25,7 @@ let player = {
 // ! 3 - Adicione ao objeto a chave bestInTheWorld e atribua a esta chave um array contendo as datas em que a jogadora Marta foi considerada a melhor do mundo.
 
 // [2006, 2007, 2008, 2009, 2010, 2018]
-player.bestInTheWold = [2006, 2007, 2008, 2009, 2010, 2018];
+// player.bestInTheWold = [2006, 2007, 2008, 2009, 2010, 2018];
 // console.log(player.bestInTheWold);
 // console.log(player);
 
@@ -36,4 +36,53 @@ player.bestInTheWold = [2006, 2007, 2008, 2009, 2010, 2018];
 
 // ! 5 - Acesse a chave medals e faça um console.log no seguinte formato: "A jogadora possui 2 medalhas de ouro e 3 medalhas de prata".
 
-console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.');
+// console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.');
+
+//------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
+
+// PARA FIXAR  "FOR/IN" //
+
+// let cars = ['Saab', 'Volvo', 'BMW'];
+
+// for (let index in cars) {
+//   console.log(cars[index]);
+// }
+
+// let car = {
+//     type: 'Fiat',
+//     model: '500',
+//     color: 'white',
+//   };
+  
+//   for (let index in car) {
+//     console.log(index, car[index]);
+//   }
+// ! 1 - Usando o objeto abaixo, utilize For/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+
+/* let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge' 
+};
+
+for (let index in names){
+    console.log('Olá ' + names[index]);
+} */
+
+// ! 2 - Usando o objeto abaixo, utilize For/in e imprima um console.log com as chaves e valores desse objeto.
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+  for (let index in car){
+      console.log(index + ': ' + car[index]);
+  }
