7cc030d58f863922b87de47278ebc192420fd60e
Daniel Saraiva <danieldsvn@gmail.com>
2021-12-01 18:26:05 -0300

    .gitignore criado

--- fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-04-java-script-objeto-e-funcoes/exercicios.js
+++ fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-04-java-script-objeto-e-funcoes/exercicios.js
@@ -3,22 +3,33 @@ Agora vamos fazer alguns exercícios de fixação para consolidar os conheciment
 
 // ! 1 - Crie um objeto player contendo as variáveis listadas abaixo.
 
-let name = 'Marta';
-let lastName = 'Silva';
-let age = 34;
-let medals = { golden: 2, silver: 3 };
+// let name = 'Marta';
+// let lastName = 'Silva';
+// let age = 34;
+// let medals = { golden: 2, silver: 3 };
 
-
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: '34',
+    medals: {
+        golden: 2,
+        silver: 3,
+    },
+};
 
 // ! 2 - Acesse as chaves name , lastName e age e concatene as suas informações para imprimir no console uma mensagem no seguinte formato: "A jogadora Marta Silva tem 34 anos de idade".
 
-
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade');
 
 
 
 // ! 3 - Adicione ao objeto a chave bestInTheWorld e atribua a esta chave um array contendo as datas em que a jogadora Marta foi considerada a melhor do mundo.
 
 // [2006, 2007, 2008, 2009, 2010, 2018]
+
+player.bestInTheWold = [2006, 2007, 2008, 2009, 2010, 2018];
+console.log(player.bestInTheWold);
 
 
 
