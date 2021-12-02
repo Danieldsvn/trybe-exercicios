25c100d3ec96d52aab5f1ed478d4c9b95350cb4a
Daniel Saraiva <danieldsvn@gmail.com>
2021-12-02 13:15:52 -0300

    4 primeiros exercicios da parte 1 concluidos

--- fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-04-java-script-objeto-e-funcoes/exercicios.js
+++ fundamentos/bloco-04-introducao-a-java-script-e-logica-de-programacao/dia-04-java-script-objeto-e-funcoes/exercicios.js
@@ -156,18 +156,110 @@ function resto(a, b) {
 
 // ! 5. Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
 
-function triAngulos(a, b, c){
-    if (a < 0 || b < 0 || c < 0) {
-        return 'angulo(os) invalidos';
-    }
-    else {
-        if ( a + b + c !== 180) {
-            return false;
-        }
-        else {
-            return true;
-        }
-    }
-}
+// function triAngulos(a, b, c){
+//     if (a < 0 || b < 0 || c < 0) {
+//         return 'angulo(os) invalidos';
+//     }
+//     else {
+//         if ( a + b + c !== 180) {
+//             return false;
+//         }
+//         else {
+//             return true;
+//         }
+//     }
+// }
 
-console.log(triAngulos(-60, 60, 60));
+// console.log(triAngulos(-60, 60, 60));
+
+//-----------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
+
+//* EXERCÍCIOS DO DIA //
+
+// Parte I - Objetos e For/In
+// Usando o objeto abaixo, faça os exercícios a seguir:
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+// !1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
+
+// console.log('Boas vindas, ' + info.personagem);
+
+// Bem-vinda, Margarida
+
+// ! 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+
+
+info.recorrente = 'Sim';
+// console.log(info);
+
+
+// {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   recorrente: 'Sim'
+// }
+// ! 3 - Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+
+// personagem
+// origem
+// nota
+// recorrente
+
+// for (let key in info) {
+//     console.log(key);
+// }
+// ! 4 - Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+
+// Margarida
+// Pato Donald
+// Namorada do personagem principal nos quadrinhos do Pato Donald
+// Sim
+
+for (let value in info) {
+    console.log(info[value]);
+}
+// ! 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
+
+// Margarida e Tio Patinhas
+// Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178
+// Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas
+// Ambos recorrentes // Atenção para essa última linha!
+// Usando o objeto abaixo, faça os exercícios a seguir:
+
+// let leitor = {
+//   nome: 'Julia',
+//   sobrenome: 'Pessoa',
+//   idade: 21,
+//   livrosFavoritos: [
+//     {
+//       titulo: 'O Pior Dia de Todos',
+//       autor: 'Daniela Kopsch',
+//       editora: 'Tordesilhas',
+//     },
+//   ],
+// };
+// !6 - Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos , e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+
+// !7 - Adicione um novo livro favorito na chave livrosFavoritos , que é um array . Atribua a esta chave um objeto contendo as seguintes informações:
+
+// {
+//   titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+//   autor: 'JK Rowling',
+//   editor: 'Rocco',
+// }
+
+//! 8 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
+
+
