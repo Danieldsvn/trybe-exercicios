74625b80118e3dd9ecfe4868fac7d6191b947e09
Daniel Saraiva <danieldsvn@gmail.com>
2021-12-06 19:30:03 -0300

    exercicio 1 ao 5 concluido.

--- fundamentos/bloco-05-java-script-dom-eventos-web-storage/dia-2-java-script-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-05-java-script-dom-eventos-web-storage/dia-2-java-script-trabalhando-com-elementos/script.js
@@ -1,5 +1,5 @@
 // arquivo script.js
 
-console.log(document.getElementById('start').nextSibling) // nó
+// console.log(document.getElementById('start').nextSibling) // nó
 
-console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>
+// console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>
