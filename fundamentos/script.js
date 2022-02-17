9c3ae5ed2b87a7070804a59fe679440f4a7efbf2
Daniel Saraiva <danieldsvn@gmail.com>
2022-02-17 00:42:20 -0300

    Deixei os exercicios de lado...

---
+++ fundamentos/script.js
@@ -0,0 +1,41 @@
+// // Try and Catch. Capturando erros no javascript
+
+
+
+// const verifyIsNumber = (value1, value2) => {
+//   if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+//     throw new Error('Os valores devem ser numéricos');
+//   }
+// };
+
+// const sum = (value1, value2) => {
+//   try {
+//     verifyIsNumber(value1, value2);
+//     return value1 + value2;
+//   } catch (error) {
+//     throw error.message;
+//   }
+// };
+
+// console.log(sum(2, '3'));
+
+// const customer = {
+//   firstName: 'Roberto',
+//   age: 22,
+//   job: 'Teacher',
+// };
+
+// let newKey = 'lastName';
+// const lastName = 'Ferreira';
+// customer[newKey] = lastName;
+// newKey = 'fullName';
+// const fullName = `${customer.firstName} ${customer.lastName}`;
+// customer[newKey] = fullName;
+// console.log(customer);
+
+const objeto = (object, key, value) => {
+   object[key] = value;
+   return object;
+}
+let newObject = new Object();
+objeto(newObject, "chave", "valor");
