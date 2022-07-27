f75d543553506c8fc3ed0e68a398b4cc53bdf181
Daniel Saraiva <danieldsvn@gmail.com>
2022-07-27 20:31:32 -0300

    Exercicio 1 feito

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/index.js
@@ -0,0 +1,16 @@
+
+function soma (num1, num2, num3) {
+  const promise = new Promise((resolve, reject) => {
+    if(typeof num1 !== "number" || typeof num2 !== "number" || typeof num3 !== "number" ) {
+      reject(new Error("Informe apenas números"));
+    };
+    const resultado = (num1 + num2) * num3;
+    if(resultado < 50) reject(new Error('Valor muito baixo'));   
+    resolve(resultado);
+  })
+  return promise;  
+}
+
+soma(2,2,46)
+  .then(result => console.log(result))
+  .catch(err => console.log("Erro:", err.message))
