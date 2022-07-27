f91540978cfb025d6ef576148ae0a4b8b00e2b04
Daniel Saraiva <danieldsvn@gmail.com>
2022-07-26 21:23:36 -0300

    Cria calculadora simples usando Node.js

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/index.js
@@ -0,0 +1,47 @@
+const readline = require('readline-sync');
+const { message1, message2, message3 } = require('./messages');
+
+console.log('CALCULADORA BASICA');
+
+function calculadora() {
+  const primeiroNumero = readline.questionInt(message1());
+  const operador = readline.question(message2());
+  const segundoNumero = readline.questionInt(message3());  
+  switch(operador) {
+    case '+':
+    case 'mais':
+    case 'adição':
+      console.log('Resultado');
+     console.log(primeiroNumero + segundoNumero);
+    break;
+    case '-':
+    case 'menos':
+    case 'subtração':
+      console.log('Resultado');
+      console.log(primeiroNumero - segundoNumero);
+      break;
+    case '*':
+    case 'vezes':
+    case 'multiplicação':
+      console.log('Resultado');
+      console.log(primeiroNumero * segundoNumero);
+      break;
+    case '/':
+    case 'dividido':
+    case 'divisão':
+      console.log('Resultado');
+      console.log(primeiroNumero / segundoNumero);
+      break;
+    default: 
+    console.log('Operador errado!');     
+  }  
+};
+
+calculadora();
+
+// const name = readline.question('Qual seu nome? ');
+// const age = readline.questionInt('Qual sua idade? ');
+
+// console.log('hello World!');
+
+// console.log(`Hello, ${name}! You are ${age} years old!`);
---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/messages.js
@@ -0,0 +1,9 @@
+const message1 = () => console.log('Digite o primeiro número: ');
+const message2 = () => console.log('Digite o operador: ');
+const message3 = () => console.log('Digite o segundo número: ');
+
+module.exports = {
+  message1,
+  message2,
+  message3,
+};
