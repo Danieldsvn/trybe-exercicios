1db280e5308ad5df2beee9da049824c559298a96
Daniel Saraiva <danieldsvn@gmail.com>
2022-07-27 22:52:22 -0300

    Inicia exercício 4. Manipula simpsons.json gerando um array de strings

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/index.js
@@ -1,3 +1,35 @@
+const fs = require('fs');
+
+function SimpsonsCharacter(){
+  fs.readFile("./simpsons.json", 'utf-8', function(err, data){
+    if(err){
+      console.log('Não foi possível ler o arquivo');
+      return false;
+    } 
+    try {
+      const simpsonsData = JSON.parse(data);
+      const simpsonsArray = simpsonsData.map((character) => (
+        `${character.id} - ${character.name}`
+      ));
+      console.log(simpsonsArray);
+    } catch(err) {
+      console.log(`Erro: ${err}`);
+    }
+  });
+};
+
+async function main() {
+  try {
+    const result = await SimpsonsCharacter();    
+    console.log(result);
+  } catch(err) {
+    console.log(`Erro: ${err}`)
+  }
+};
+
+main();
+
+
 
 function soma (num1, num2, num3) {
   const promise = new Promise((resolve, reject) => {
@@ -20,14 +52,14 @@ const numeroAleatorio = () => Math.floor(Math.random() * 10 + 1);
 
 // consumindo promise com async await
 
-async function main(){
-  try {
-    const result = await soma(numeroAleatorio(),numeroAleatorio(), numeroAleatorio());
-    console.log(result);
-  } catch(err) {
-    console.log(`${err}`);    
-  }  
-}
+// async function main(){
+//   try {
+//     const result = await soma(numeroAleatorio(),numeroAleatorio(), numeroAleatorio());
+//     console.log(result);
+//   } catch(err) {
+//     console.log(`${err}`);    
+//   }  
+// }
 
-main();
+// main();
 
