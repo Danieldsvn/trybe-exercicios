11d6689bad2f695382dc5b2e449f247966e45e7c
Daniel Saraiva <danieldsvn@gmail.com>
2022-08-01 12:30:47 -0300

    Corrige sintaxe de respostas retirando aspas duplas

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -15,20 +15,23 @@ app.get('/ping', (request, response) => {
 app.post('/hello', (request, response) => {
   const { name } =  request.body;
   request.json(nameJson);
-  return response.status(200).json( { "message": `"Hello, ${name}` });
+  return response.status(200).json( { message: `Hello, ${name}` });
 })
 
 app.post('/greetings', (request, response) => {
   const { name, age } = request.body;
   if(+age > 17) {
-    return response.status(200).json({ "message": `"Hello, ${name}!"`});
+    return response.status(200).json({ message: `Hello, ${name}!`});
   }
   if(+age <= 17) {
-    return response.status(404).json({ "message": "Unauthorized"})
+    return response.status(404).json({ message: Unauthorized})
   }
 })
 
-app.put('')
+app.put('/users/:name/:age', (request, response) => {
+  const { name, age } = request.params;
+  return response.status(200).json({ message: `Seu nome é ${name} e você tem ${age} anos de idade`});
+})
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
