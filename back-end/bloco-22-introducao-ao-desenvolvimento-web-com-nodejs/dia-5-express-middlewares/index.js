5f4bf09510447a1a3b6a3789b28f45609387bb5b
Daniel Saraiva <danieldsvn@gmail.com>
2022-08-02 11:37:18 -0300

    remove validação de duas chaves simultaneas do objeto 'infos'

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/index.js
@@ -11,8 +11,7 @@ app.post('/sales', (request, response) => {
   if(!productName) return response.status(400).json({ message: "O campo productName é obrigatório"});
   if(productName.length < 4) return response.status(400).json({ message: "O campo productName deve ter pelo menos 4 caracteres"});
   if(!infos) return response.status(400).json({ message: "O campo infos é obrigatório"});
-  if(typeof infos !== 'object') return response.status(400).json({ message: "O campo infos é não é um objeto"});
-  if(!infos.saleDate || !infos.warrantyPeriod) return response.status(400).json({ message: "O campo infos deve possuir as chaves 'saleDate' e 'warrantyPeriod'"});
+  if(typeof infos !== 'object') return response.status(400).json({ message: "O campo infos é não é um objeto"}); 
   if(!infos.saleDate) return response.status(400).json({ message: "O campo saleDate é obrigatório"});
   const isDateValid = moment(infos.saleDate, 'DD/MM/YYYY', true).isValid();
   if(!isDateValid) return response.status(400).json({ message: "salesDate não é uma data válida"});
