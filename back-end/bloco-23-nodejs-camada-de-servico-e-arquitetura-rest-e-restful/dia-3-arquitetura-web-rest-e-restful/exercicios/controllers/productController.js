bb0a9ca4aa14104d7958b28336a68506df2f3d2f
Daniel Saraiva <danieldsvn@gmail.com>
2022-08-10 12:10:35 -0300

    Alterado nomes de endpoint, verbos http e retorno com status da aplicação para o padrão Rest.

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicios/controllers/productController.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicios/controllers/productController.js
@@ -3,38 +3,38 @@ const ProductModel = require('../models/productModel');
 
 const router = express.Router();
 
-router.get('/list-products', async (req, res) => {
+router.get('/products', async (req, res) => {
   const products = await ProductModel.getAll();
 
-  res.send(products);
+  res.status(200).json(products);
 });
 
-router.get('/get-by-id/:id', async (req, res) => {
+router.get('/products/:id', async (req, res) => {
   const product = await ProductModel.getById(req.params.id);
 
-  res.send(product);
+  res.status(200).json(product);
 });
 
-router.post('/add-product', async (req, res) => {
+router.post('/products', async (req, res) => {
   const { name, brand } = req.body;
 
   const newProduct = await ProductModel.add(name, brand);
 
-  res.send(newProduct);
+  res.status(201).json(newProduct);
 });
 
-router.post('/delete-product/:id', async (req, res) => {
+router.delete('/products/:id', async (req, res) => {
   const products = await ProductModel.exclude(req.params.id);
 
-  res.send(products);
+  res.status(204).json();
 });
 
-router.post('/update-product/:id', async (req, res) => {
+router.update('/products/:id', async (req, res) => {
   const { name, brand } = req.body;
 
   const products = await ProductModel.update(req.params.id, name, brand);
 
-  res.send(products);
+  res.status(200).json(products);
 });
 
 module.exports = router;
