60ed4a75c3820d036fb95683cb05a9518556674c
Daniel Saraiva <danieldsvn@gmail.com>
2022-09-09 11:57:07 -0300

    Exercicio de tipagem simples e criação de enum

---
+++ back-end/secao-8-typescript/dia-1-introducao-ao-typescript/my-ts-scripts/exercise2.ts
@@ -0,0 +1,17 @@
+enum Months {
+  January = 1,
+  February,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September, 
+  October,
+  November,
+  December
+}
+
+console.log(Months[1]);
+console.log(Months[12]);
