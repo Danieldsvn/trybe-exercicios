f9dc9d070a466c4ae854d012a4176abddc26ad65
Daniel Saraiva <danieldsvn@gmail.com>
2022-11-21 16:42:29 -0300

    Exercicio 2 concluido. Aplicação de herança de classes. Classe Student exstends Person

--- back-end/secao-9-programacao-orientada-a-objetos-poo-e-solid/dia-2-heranca-e-interfaces/Escola.js
+++ back-end/secao-9-programacao-orientada-a-objetos-poo-e-solid/dia-2-heranca-e-interfaces/Escola.js
@@ -30,6 +30,105 @@ class Person {
         }
     }
 }
-const person = new Person('Daniel', new Date('1923/03/03'));
-console.log(person.personName);
-console.log(person.birthday.toDateString());
+// const person1 = new Person('Daniel', new Date('1923/03/03'));
+// console.log(person1.personName);
+// console.log(person1.birthday.toDateString());
+// const person2 = new Person('Matheus', new Date('1986/04/30'));
+// console.log(person2.personName);
+// console.log(person2.birthday.toDateString());
+class Student extends Person {
+    constructor(name, birthDate) {
+        super(name, birthDate);
+        this._examsGrades = [];
+        this._worksGrades = [];
+        // this.personName = name;
+        // this.birthday = birthDate;
+        this._enrollment = this.generateEnrollment();
+        // this._examsGrades = examsGrades;
+        // this._worksGrades = worksGrades;
+    }
+    get enrollment() {
+        return this._enrollment;
+    }
+    set enrollment(value) {
+        this._enrollment = value;
+    }
+    get examsGrades() {
+        return this._examsGrades;
+    }
+    set examsGrades(value) {
+        this.examsValidation(value);
+        this._examsGrades = value;
+    }
+    get worksGrades() {
+        return this._worksGrades;
+    }
+    set worksGrades(value) {
+        this.worksValidation(value);
+        this._worksGrades = value;
+    }
+    sumGrades() {
+        const examsSum = this._examsGrades.reduce((pv, cv) => (pv + cv), 0);
+        const worksSum = this._worksGrades.reduce((pv, cv) => (pv + cv), 0);
+        return examsSum + worksSum;
+    }
+    sumAverageGrade() {
+        return this.sumGrades() / (this._examsGrades.length + this._worksGrades.length);
+    }
+    generateEnrollment() {
+        this.enrollmentValidation(this.personName + this.birthday);
+        return this.name + this.birthday;
+    }
+    enrollmentValidation(enrollment) {
+        if (enrollment.length < 16) {
+            throw new Error('Matricula com menos de 16 caracteres');
+        }
+    }
+    examsValidation(exams) {
+        if (exams.length > 4) {
+            throw new Error(`Número máximo de provas excedidos. Qtd de provas inseridas ${exams.length}. Número máximo: 4`);
+        }
+    }
+    worksValidation(works) {
+        if (works.length > 2) {
+            throw new Error(`Número máximo de provas excedidos. Qtd de provas inseridas ${works.length}. Número máximo: 2`);
+        }
+    }
+}
+const student1 = new Student('JosiCláudio', new Date(1992, 2, 1));
+console.log(student1);
+student1.examsGrades = [8, 9, 10, 7];
+student1.worksGrades = [7, 6];
+console.log(student1.sumAverageGrade());
+// const student2 = new Student('Janio', new Date(1990, 4, 13));
+// console.log(student2);
+// console.log(student2.sumAverageGrade())
+// ----------------------------------------
+// get personName(): string {
+//   return this.name;
+// }
+// set personName(value: string) {
+//   if(value.length > 3) this.name = value
+// }
+// get birthday(): Date {
+//   return this.birthDate;
+// }
+// ageGenerator(miliseconds: number): number {
+//   return Math.floor(((((miliseconds / 1000) / 60) / 60) / 24) / 365) ;
+// }
+// set birthday(value: Date) {
+//   const currentTime = new Date().getTime();
+//   const valueTime = value.getTime();
+//   const age = this.ageGenerator(currentTime - valueTime);    
+//   if(age <= 120 && age > 0) {
+//     this.birthDate = value;
+//   } else {
+//     throw new Error('Data de nascimento inválida');
+//   } 
+// }
+// const student1 = new Student('Daniel', new Date('1923/03/03'));
+// console.log(student1.personName);
+// console.log(student1.birthday.toDateString());
+// const student2 = new Student('Matheus', new Date('1986/04/30'));
+// console.log(student2.personName);
+// console.log(student2.birthday.toDateString());
